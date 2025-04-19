aee3b26ae1f56c26ffee6b27add1a81a059aae7d
dias1202 <diasdwig@gmail.com>
2025-04-19 17:44:03 +0700

    add performance and security

--- app/src/main/java/com/dicoding/gamerzoneapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/dicoding/gamerzoneapp/ui/detail/DetailViewModel.kt
@@ -1,6 +1,5 @@
 package com.dicoding.gamerzoneapp.ui.detail
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
